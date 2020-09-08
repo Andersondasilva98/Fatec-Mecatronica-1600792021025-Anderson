2cd40290178962f011db73533a514a5696cbd043
Andersondasilva98 <70581878+Andersondasilva98@users.noreply.github.com>
2020-09-08 09:31:48 -0300

    Create programa04.py

---
+++ LTP1-2020-2/Pratica03/programa04.py
@@ -0,0 +1,5 @@
+#pede para o usuario dois numeros e realiza a soma deles
+numero1 = int(input('Informe um valor:'))
+numero2 = int(input('Informe outro valor:'))
+resultado = nuemro1 + numero2
+print('Resultado da soma:', resultado)
